65a87512c644df058922c4bb9e1d14060703845d
yeonkkk <tjddus0302@gmail.com>
2022-09-20 20:23:23 +0900

    18: SecurityConfig 생성

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티 활성화
+@Configuration // IoC
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http); 기존 시큐리티 기능 모두 비활성화
+        http.authorizeRequests()
+                .antMatchers("/", "/user/**", "/inage/**", "/subscribe/**", "/comment/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/auth/signin")
+                .defaultSuccessUrl("/");
+    }
+}
